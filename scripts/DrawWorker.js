902b928868f8ff82d19a27298ae036c8179aa651
Grahamworth <grahamworth@vdaso.com>
2016-08-08 14:28:24 +0100

    Update

---
+++ scripts/DrawWorker.js
@@ -0,0 +1,20 @@
+var canvas = "";
+
+function DrawWorker() {
+    for (var j = 5; j > 1; j--) {
+        var ctx = canvas.getContext("2d");
+        ctx.beginPath();
+        var x = 200 + (j * 20);               // The x-coordinate.
+        var y = 200;               // The y-coordinate. 
+        var radius = 50;                    // The arc radius.
+        var startAngle = 0;                     // The starting point on the circle.
+        var endAngle = Math.PI + (Math.PI); // The end point on the circle.
+        var anticlockwise = true; // The direction of drawing.
+        ctx.lineWidth = 8;
+        ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise); // Create the arc path.
+        ctx.stroke();
+        setTimeout(function () { }, 1000);
+    }
+}
+
+DrawWorker();
